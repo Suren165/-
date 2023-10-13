e47cbb989b9ada0d408d1b68cccc390029915f0e
Suren Arakelyan <suren.arakelyan@elev.ga.ntig.se>
2023-10-13 12:44:51 +0200

    hej

--- Hund/Program.cs
+++ Hund/Program.cs
@@ -11,18 +11,24 @@ myHund.name = Console.ReadLine();
 
 Console.WriteLine($"{myHund.name} är ett fint namn!");
 
+Console.Clear();
 
 while (myHund.GetAlive() == true)
 {
-    Console.Clear();
     myHund.PrintStats();
     Console.WriteLine("Vad vill du göra");
-    Console.WriteLine($"1.Lär {myHund.name} ett nytt ord ");
-    Console.WriteLine($"2.Prata med {myHund.name}");
-    Console.WriteLine($"3. mata {myHund.name}");
-    Console.WriteLine($"4. inget");
-   
+    Console.WriteLine($"1. ge mat till {myHund.name}");
+    Console.WriteLine($"2.Lär {myHund.name} ett nytt ord");
+    Console.WriteLine($"3. Prata med {myHund.name}");
+
+    Console.ReadLine();
+    Console.Clear();
 }
+string gora = Console.ReadLine();
+
+Console.ReadLine();
+
+
 
 
 
--- Hund/classer.cs
+++ Hund/classer.cs
@@ -6,17 +6,17 @@ using System.Collections.Generic;
 
 public class Hund
 {
-private int hunger;
-private int boredom;
+  private int hunger;
+  private int boredom;
 
-private List <string> words = new List <string>() {"voff"};
-private bool isAlive;
-private Random generator;
+  private List<string> words = new List<string>() { "voff" };
+  private bool isAlive;
+  private Random generator;
 
-public string name;
+  public string name;
 
 
-public Hund()
+  public Hund()
   {
     generator = new Random();
     isAlive = true;
@@ -24,23 +24,80 @@ public Hund()
 
   public void feed()
   {
-Console.WriteLine($"[{name}] Jag är full nu tack ");  
+    Console.WriteLine($"[{name}] Jag är full nu tack ");
 
-hunger -=3;
-if (hunger > 0);
+    hunger -= 2;
+    if (hunger < 0)
+    {
+      hunger = 0;
+      Console.WriteLine($"{name} är redan mätt!");
+    }
+  }
+
+
+  public bool GetAlive()
+  {
+    if (isAlive)
+    {
+      return true;
+    }
+    return false;
+  }
+
+  public void Hi()
+  {
+    int wordNum = generator.Next(words.Count);
+    Console.WriteLine($" [{name}] säger: {words[wordNum]}");
+    ReduceBoredom();
+  }
+
+  public void Teach(string word)
+  {
+    Console.WriteLine($" [{name}] lär: {word}");
+    words.Add(word);
+    ReduceBoredom();
+  }
+public void Tick()
 {
-  hunger = 0;
+  hunger++;
+  boredom++;
+  if (hunger > 10 || boredom > 10)
+  {
+    isAlive = false;
+  }
 }
-}
+
+
+  public void PrintStats()
+  {
+    Console.WriteLine($"Hunger: {hunger}");
+    Console.WriteLine($"Uttråkad:{boredom}");
+  }
+
+  public void ReduceBoredom()
+  {
+    Console.WriteLine($"Nu är glad [{name}] ");
+    boredom -= 2;
+    if (boredom < 0)
+    {
+      boredom = 0;
+    }
+  }
+
 
 }
 
 
 
-  
 
 
 
 
- 
+
+
+
+
+
+
+
 
