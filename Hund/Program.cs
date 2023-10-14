691410f6122172edb07c7c320d5858626dd84067
Suren Arakelyan <suren.arakelyan@elev.ga.ntig.se>
2023-10-14 11:10:46 +0200

    Hund klar

--- Hund/Program.cs
+++ Hund/Program.cs
@@ -20,11 +20,35 @@ while (myHund.GetAlive() == true)
     Console.WriteLine($"1. ge mat till {myHund.name}");
     Console.WriteLine($"2.Lär {myHund.name} ett nytt ord");
     Console.WriteLine($"3. Prata med {myHund.name}");
+    Console.WriteLine($"4. Inget");
 
     Console.ReadLine();
     Console.Clear();
-}
+
 string gora = Console.ReadLine();
+if (gora == "1")
+{
+    Console.WriteLine("vilket ord vill du lära mig");
+    string word = Console.ReadLine();
+    myHund.Teach(word);
+}
+ if(gora == "2")
+{
+   myHund.Hi();
+}
+    if (gora == "3")
+    {
+        myHund.feed();
+    }
+    else
+    {
+      Console.WriteLine("Gör inget  zzzzzzzz");
+    }
+    myHund.Tick();
+}
+
+Console.WriteLine($" {myHund.name} är död!");
+Console.WriteLine("Tryck på enter för att lämna");
 
 Console.ReadLine();
 
